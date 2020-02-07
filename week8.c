d5e1c2e1dcc78fed560d014b0d0a8c3a28c3e472
Boss Nantapat <nantapat.y@ku.th>
2020-02-07 20:26:39 +0700

    add file week 8

---
+++ week8.c
@@ -0,0 +1,11 @@
+//คำสั่ง git
+//git status ตรวจสอบว่ามีไฟล์อะไรบ้างที่สามารถ add เข้าไปได้
+//git add ใช้เพิ่มไฟล์
+//git commit --global user.name "ชื่อของเรา"
+//git commit --global user.email "email ของเรา"
+//ถ้าเป็นเครื่องส่วนรวมให้เอา --global ออก
+//ทำเพียงครั้งเดียว
+//git commit -m "ข้อความอธิบาย"
+//git remote add (ชื่อ remoteแต่ส่วนใหญ่จะเป็นorigin) (git url)(แล้วแต่เจ้าของเว็บแต่ของผมจะเป็นgit hub)
+//git push -u origin master เป็นการนำไฟล์ที่แอดเข้าสู่ เว็บที่เชื่อมไว้กับgit url
+//git log --all --decorate --oneline --graph ดูcheck point ต่างๆที่เก็บไว้ใน origin master
